9ffc0a23245987cd95089cddeaa888e666f998db
str1ngs <mike.rosset@gmail.com>
2012-04-02 04:35:16 -0700

    call functions from arg1, use _test

--- gohub/main.go
+++ gohub/main.go
@@ -1,26 +1,79 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"github.com/str1ngs/gohub"
+	"github.com/str1ngs/util/console"
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 var (
-	user  = "str1ngs"
-	repos []gohub.Repo
+	user = "str1ngs"
+	home = "/home/strings/gocode/src/github.com/str1ngs"
 )
 
+var commands = []*Command{
+	&Command{"clone", clone},
+	&Command{"check", check},
+}
+
+type Command struct {
+	Name string
+	Run  func(args []string)
+}
+
 func init() {
+	log.SetPrefix("gohub: ")
+	log.SetFlags(log.Lshortfile)
 }
 
 func main() {
-	var err error
-	repos, err = gohub.Repos(user)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		flag.Usage()
+	}
+	for _, cmd := range commands {
+		if cmd.Name == args[0] {
+			cmd.Run(args[1:])
+		}
+	}
+}
+
+func clone(args []string) {
+	repos, err := gohub.Repos(user)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = gohub.CloneAll("/home/strings/gocode/src/github.com/", repos)
+	err = gohub.CloneAll(home, repos)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func check(args []string) {
+	glob := filepath.Join(home, "*")
+	dirs, err := filepath.Glob(glob)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, d := range dirs {
+		buf := new(bytes.Buffer)
+		git := exec.Command("git", "status", "--porcelain")
+		git.Stdout = buf
+		git.Stderr = os.Stderr
+		git.Dir = d
+		err = git.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(buf.Bytes()) > 0 {
+			console.Println(filepath.Base(d), "*")
+		}
+	}
+	console.Flush()
 }
